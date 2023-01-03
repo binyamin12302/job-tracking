292480bbedf3b41e7342e6f2416877831cea4cba
binyamin12302 <binyamin12302@gmail.com>
2023-01-03 12:16:05 +0200

    added setup production in package.json

--- Backend/server.js
+++ Backend/server.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV !== 'production') {
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 
-app.use(express.static(path.resolve(__dirname, './build')));
+app.use(express.static(path.resolve(__dirname, '../Frontend/build')));
 app.use(express.json());
 app.use(helmet());
 app.use(xss());
@@ -45,14 +45,13 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, '../Frontend/build', 'index.html'))
 });
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
-
 
 const start = async () => {
   try {
